40cd5c0168536b80e12e1da744da3b319dddfdb0
jstarkov <js486@hdm-stuttgart.de>
2023-10-18 16:06:48 +0200

    Commit

--- src/main/java/mainpackage/Controller.java
+++ src/main/java/mainpackage/Controller.java
@@ -3,10 +3,40 @@ package mainpackage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-public class Controller {
+import java.util.Scanner;
 
-    @FXML
-    private void sayHello(ActionEvent event) {
-        System.out.println("Hello!");
+public class Controller {
+    //@FXML
+   // private void sayHello(ActionEvent event) {
+    public void controller(){
+
+
+        Scanner scanner = new Scanner(System.in);
+        Kontoübersicht kontoübersicht = new Kontoübersicht();
+        Konto konto = new Konto();
+
+
+        System.out.println("Welche Aktion wollen Sie durchführen? \n" +
+                "1: Kontostand anzeigen\n" +
+                "2: Einnahme hinzufügen\n" +
+                "3: Ausgabe hinzufügen\n");
+
+        int aktion = scanner.nextInt();
+
+        switch (aktion){
+            case 1:
+                System.out.println(konto.Kontostand);controller();break;
+            case 2:
+                konto.Einnahmen();break;
+            case 3:
+                konto.Ausgaben();break;
+            default:
+                System.err.println("Error");
+        }
+
+
     }
+
+
+
 }
