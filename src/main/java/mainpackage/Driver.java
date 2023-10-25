2cd4cfe2f3a32e89d19585929d2d555ad7596c58
Joel Starkov <js486@hdm-stuttgart.de>
2023-10-25 13:01:36 +0200

    Verbindung mit Datenbank Test

--- src/main/java/mainpackage/Driver.java
+++ src/main/java/mainpackage/Driver.java
@@ -1,14 +1,18 @@
 package mainpackage;
 
 
+import Datenbank.Kontodaten;
+
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Driver {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Controller controller = new Controller();
-
-        controller.controller();
+        Kontodaten kontodaten = new Kontodaten();
+        kontodaten.Kontoabfrage();
+        //controller.controller();
 
     }
 }
